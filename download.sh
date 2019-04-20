9b0c4f4dbf2e98a2240d2514ad08895a29ef2974
Abhinish13 <abhinish.gecr@gmail.com>
2019-04-20 08:13:43 +0530

    Packages Added #1

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PACKAGES=$1
+
+for i in $(apt-cache depends ${PACKAGES} | grep -E 'Depends|Recommends|Suggests' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do sudo apt-get download $i 2>>errors.txt; done
