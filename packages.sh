eb8bca8e800c0fbda6737137f3fb0b8eb76b53e6
Your Name <you@example.com>
2019-04-24 05:20:09 +0000

    Packages Added #5

--- packages.sh
+++ packages.sh
@@ -1,1 +1,12 @@
-apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends xrdp | grep "^\w" | sort -u)
+#!bin/bash
+
+PACKAGES=$1
+
+mkdir ${PACKAGES}_DEB
+
+cd ${PACKAGES}_DEB
+
+
+apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances --no-pre-depends ${PACKAGES} | grep "^\w" | sort -u) && echo "${PACKAGES} download"
+
+cd ..
